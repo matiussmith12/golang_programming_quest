d750fc5ec68fa62a98a59fb3cc995f8b3b51d073
Messi <adrianpros27@gmail.com>
2022-07-05 17:12:15 +0700

    reverse string quest

--- test_dev/soal2/main.go
+++ test_dev/soal2/main.go
@@ -11,7 +11,20 @@ func reverse(s string) (result string ){
 	return result
 }
 
+func reverseWord(s string) string {
+    rns := []rune(s) // convert to rune
+    for i, j := 0, len(rns)-1; i < j; i, j = i+1, j-1 {
+  
+        // swap the letters of the string,
+        // like first with last and so on.
+        rns[i], rns[j] = rns[j], rns[i]
+    }
+  
+    // return the reversed string.
+    return string(rns)
+}
 
 func main() {
 	fmt.Println(reverse("adrian"))
+	fmt.Println(reverseWord("messi"))
 }
