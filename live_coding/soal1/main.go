83a71c963525b1f5367ea319e9eea6b2e7ffaf2d
Messi <adrianpros27@gmail.com>
2022-07-06 17:09:10 +0700

    sieve prime quest

---
+++ live_coding/soal1/main.go
@@ -0,0 +1,42 @@
+package main
+
+import "fmt"
+
+func Max(x, y int) int {
+	if x < y {
+		return y
+	}
+	return x
+}
+
+func findMax(nums []int, k int) int {
+
+	// Checking array length
+	if len(nums) < k {
+		fmt.Println("Length of array is smaller than k")
+		return -1
+	}
+
+	sum := 0
+	// Initiated maximum sum of K size
+	for i := 0; i < k; i++ {
+		sum += nums[i]
+	}
+
+	ans := sum
+	// Checking sum after kth index
+	for i := k; i < len(nums); i++ {
+		ans += nums[i] - nums[i-k]
+		sum = Max(sum, ans)
+	}
+
+	return sum
+}
+
+func main() {
+	arr := []int{2, 1, 3, 4, 5, 6}
+	k := 3
+
+	answer := findMax(arr, k)
+	fmt.Println(answer)
+}
